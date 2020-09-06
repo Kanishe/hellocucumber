6a4bfbca822c7561d77a7f868016b248372dae9e
Alexander Kanishevskii <a.kan4988@gmail.com>
2020-09-06 20:25:56 +0300

    fix name class for test correct email

--- src/test/java/com/cucumber/steps/OtusContactEmailStepdefs.java
+++ src/test/java/com/cucumber/steps/OtusContactEmailStepdefs.java
@@ -6,7 +6,7 @@ import io.cucumber.java.en.When;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class OtusGetInfo {
+public class OtusContactEmailStepdefs {
 
     @When("When I select tab {string}")
     public void SelectTabName(String tabName) {
